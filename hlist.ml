add12bddb4e5e1becf0d1ce0f01d02d32a3d40d6
Runhang Li <runhang@posteo.de>
2015-11-21 14:14:19 -0800

    Fix weak polymorphism

--- hlist.ml
+++ hlist.ml
@@ -1,24 +1,18 @@
-(* One serious problem usually arises: weak polymorphism *)
-
 module HList = struct
   type (_, _) hlist =
     | Empty : ('a, 'a) hlist
-    | Cons  : 'c * ('a, 'b) hlist -> ('c -> 'a, 'b) hlist
+    | :: : 'c * ('a, 'b) hlist -> ('c -> 'a, 'b) hlist
 
   let cons : type a b c. c -> (a, b) hlist -> (c -> a, b) hlist =
-    fun h tl -> Cons (h, tl)
+    fun h tl -> h :: tl
 
   let hd : type a b c. (c -> a, b) hlist -> c option = function
     | Empty -> None
-    | Cons (c, _) -> Some c
+    | c :: _ -> Some c
 
   let tl : type a b c. (c -> a, b) hlist -> (a, b) hlist option = function
     | Empty -> None
-    | Cons (_, tl) -> Some tl
-
-  let empty = Empty
-
-  let ( **> ) = cons
+    | _ :: tl -> Some tl
 
   type iterf = {
     iter : 'a. 'a -> unit;
@@ -27,12 +21,13 @@ module HList = struct
   let rec iter : type a b. iterf -> (a, b) hlist -> unit = fun iterf l ->
     match l with
     | Empty -> ()
-    | Cons (c, tl) -> iterf.iter c; iter iterf tl
+    | c :: tl -> iterf.iter c; iter iterf tl
 
-  let l : (char -> string -> unit, unit) hlist = '2' **> "str" **> empty
+  let l = '2' :: "str" :: Empty
 
   let rec append : type a b c d. (a, b) hlist -> (b, d) hlist -> (a, d) hlist = fun l1 l2 ->
     match l1 with
     | Empty -> l2
-    | Cons (hd, tl) -> Cons (hd, (append tl l2))
+    | hd :: tl -> hd :: (append tl l2)
 end
+
