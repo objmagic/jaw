d6664f5bc61001b9d5c4fed65389021a869113fe
Jean-Noel Avila <jn.avila@free.fr>
2016-07-24 18:30:14 +0200

    Add safe_list map2
    This is the very purpose of safe lists, isn't it ?

--- safe_list.ml
+++ safe_list.ml
@@ -5,3 +5,10 @@ type ('a,_) t =
 let rec smap: type l. ('a -> 'b) -> ('a, l) t -> ('b, l) t = fun f -> function
   | Nil -> Nil
   | Cons (h, t) -> Cons (f h, smap f t)
+
+let rec smap2: type l. ('a -> 'b -> 'c) -> ('a, l) t -> ('b, l) t -> ('c, l) t =
+  fun f a b ->
+  match b with
+  | Nil -> Nil
+  | Cons (hb,tb) ->
+     let  Cons (ha, ta) = a in Cons (f ha hb , smap2 f ta tb)
